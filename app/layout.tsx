be89af8cb89672bc2bf33da415850305cac41dd4
Oscar2006 <oskarascharciuk@gmail.com>
2025-04-06 10:52:07 +0300

    Implement Navbar and Dark Theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Navbar } from "@/components/Navbar";
 
 export const metadata: Metadata = {
   title: "ArturHub",
@@ -12,8 +14,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <>
-      <h1>Hello?</h1>
+      <h1 className="text-3xl flex justify-center items-center h-[100px]">
+        Welcome to the Home Page!
+      </h1>
     </>
   );
 }
